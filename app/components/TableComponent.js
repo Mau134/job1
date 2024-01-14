bf4fa3b07dd2024e9efa42eccaa66b2d60832af2
Maurice Molande <molande.mau@gmail.com>
2024-01-14 19:56:00 +0200

    initial commit

---
+++ app/components/TableComponent.js
@@ -0,0 +1,68 @@
+'use client'
+// components/TableComponent.js
+import React, { useMemo } from 'react';
+import { useTable } from 'react-table';
+
+const TableComponent = () => {
+  const data = useMemo(
+    () => [
+      { name: 'Maurice Molande', age: 25, email: 'mau.molande@gmail.com' },
+      { name: 'Nohata Molande', age: 30, email: 'nohata.molande@gmail.com' },
+      // Add more data as needed
+    ],
+    []
+  );
+
+  const columns = useMemo(
+    () => [
+      { Header: 'Name', accessor: 'name' },
+      { Header: 'Age', accessor: 'age' },
+      { Header: 'Email', accessor: 'email' },
+    ],
+    []
+  );
+
+  // Create an instance of the useTable hook
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns, data });
+
+  return (
+    <table {...getTableProps()} style={{ borderCollapse: 'collapse', width: '100%' }}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr
+            {...headerGroup.getHeaderGroupProps()}
+            style={{ borderBottom: '2px solid #ddd' }}
+          >
+            {headerGroup.headers.map((column) => (
+              <th
+                {...column.getHeaderProps()}
+                style={{ padding: '10px', background: '#394648' }}
+              >
+                {column.render('Header')}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr
+              {...row.getRowProps()}
+              style={{ borderBottom: '1px solid #ddd' }}
+            >
+              {row.cells.map((cell) => (
+                <td {...cell.getCellProps()} style={{ padding: '5px' }}>
+                  {cell.render('Cell')}
+                </td>
+              ))}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default TableComponent;
